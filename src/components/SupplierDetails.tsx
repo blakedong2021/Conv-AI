1553bc36355cf2e62144f7ed7ee0a61d9120b6ee
Victor Martinez <vmartinez@pointb.com>
2022-03-04 09:28:34 -0700

    WIP details pane

--- src/components/SupplierDetails.tsx
+++ src/components/SupplierDetails.tsx
@@ -1,21 +1,48 @@
 import * as React from 'react';
 import {
   Box,
+  Container,
+  Chip,
   Paper,
   Skeleton,
+  Stack,
+  Typography,
  } from '@mui/material';
  import ExpansionPlaceholder from '../assets/supplier_expand.png';
  
 export default function ProductSummary() {
   return (
-    <Box
-      component="img"
-      sx={{
-        pl: 4,
-        pr: 4,
-      }}
-      alt="SUPPLIER ROUTES"
-      src={ExpansionPlaceholder}
-    />
+      <React.Fragment>
+          <Box
+              component="img"
+              sx={{
+                  pl: 4,
+                  pr: 4,
+              }}
+              alt="SUPPLIER ROUTES"
+              src={ExpansionPlaceholder}
+          />
+          <Container>
+              <Stack direction="row" spacing={4}>
+                  <Stack spacing={2}>
+                      <Chip label="Distance" />
+                      <p>Distance</p>
+                  </Stack>
+                  <Stack spacing={2}>
+                      <Chip label="Distance" />
+                      <p>Total Weight</p>
+                  </Stack>
+                  <Stack spacing={2}>
+                      <Chip label="Distance" />
+                      <p>Trip Duration</p>
+                  </Stack>
+                  <Stack spacing={2}>
+                      <Chip label="Distance" />
+                      <Typography align="center">Total Carbon Cost</Typography>
+                  </Stack>
+              </Stack>
+
+          </Container>
+      </React.Fragment>
   );
 }
